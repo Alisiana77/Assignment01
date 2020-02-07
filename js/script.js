6dd5380138c85ef121e598305bcb400e01d34886
Alisiana77 <60444717+Alisiana77@users.noreply.github.com>
2020-02-06 16:53:51 -0800

    Initial assignment 1

---
+++ js/script.js
@@ -0,0 +1,190 @@
+// STEP 1 ********
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2
+//  345    numeric
+//  "year" string
+//  true   Boolean
+//  null   Null
+
+// STEP 3 
+// let total = (net * discountPercent) + tax;
+// let newTotal = total - discountTotal + "$";
+
+// STEP 4
+// let firstName;
+// let lastName;
+// let adress;
+// let city;
+// let state;
+// let zipCode;
+// let yourAge;
+// let referralSource;
+// let mayWeContactYou;
+
+
+// STEP 5 
+/* 
+let firstName = "JOhn";
+let lastName = "Doe";
+let adress = " 21 Jump Street";
+
+let city;
+let state;
+let zipCode;
+city = "San Diego";
+state = "CA";
+zipCode = 92111;
+
+
+let yourAge = 30, referralSource = "Internet", mayWeContactYou = "No"; 
+
+
+*/
+
+// STEP 6
+/* 
+let count = 10;
+window.console.log("The total count is" + count);
+*/
+
+// STEP 7
+/*
+let a = false;
+let b = 30;
+window.console.log(a + "Student pass the test.");
+window.console.log(b + a);
+*/
+
+// STEP 8
+// No the string is not valid, the correct way is in below comment
+/*
+var someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\"";
+window.console.log(someString);
+*/
+
+// STEP 9
+/*
+let string;
+let x = null;
+window.console.log(x);
+window.console.log(string);
+*/
+
+// STEP 10 *******
+/*
+"year" 
+let c = 345;    
+
+true   
+null
+window.console.log(typeof c);
+window.console.log(typeof "year");
+window.console.log(typeof 345);
+window.console.log(typeof 345);
+
+*/
+
+// STEP 11
+ //window.alert("Hello " + "Alisa," + " Welcome to the Javascript class!");
+
+// STEP 12
+
+// let name = "Alisa";
+// window.alert("Hello " + name + " Welcome to the Javascript class!");
+
+
+// STEP 13 *****
+/*
+let name = "Alisa";
+let course = "JavaScript";
+window.alert("Hello " + name + " Welcome to the " + course + " class!");
+*/
+
+
+// STEP 14
+/*
+let name = "Alisa";
+let course = "JavaScript";
+window.alert("Hello " + name + "\n" + "Welcome to the " + course + " class!");
+*/
+
+// STEP 15
+/*
+let name = window.prompt("What's your name?");
+let course = "JavaScript";
+window.alert("Hello " + name + "\n" + "Welcome to the " + course + " class!");
+*/
+
+// STEP 16 *****
+/*
+let name = window.prompt("What's your name?");
+let course = prompt("What class are yoou taking?");
+window.alert("Hello " + name + "\n" + "Welcome to the " + course + " class!");
+*/
+
+// STEP 17
+/*
+let x = 10;
+let y = 20;
+window.console.log(x + y);
+*/
+
+// STEP 18
+/*
+let x = 20;
+x += 20;
+window.console.log(x);
+*/
+
+// STEP 19
+/*
+let x = 20;
+x *= 5;
+window.console.log(x);
+*/
+
+// STEP 20 ***********
+/*
+let x = (20 % 3);
+x /= 1;
+window.console.log(x);
+*/
+
+// STEP 21
+/*
+let x = 10;
+let y = 20;
+window.console.log(x == 10 && y == 20);
+*/
+
+// STEP 22
+/*
+let a = 200;
+let b = 350;
+window.console.log(a > b || b == a);
+*/
+
+// STEP 23
+/*
+let widget = new Object();
+window.console.log(typeof widget);
+*/
+
+// STEP 24  ******
+
+/*
+let widget = new Object();
+window.console.log(widget instanceof Object);
+*/
+
+// STEP 25
+
+/*
+let widget = new Object();
+window.console.log(x instanceof String);
+*/
